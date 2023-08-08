7df42d809d9a17d262b3cf8763e12ba0a3770eed
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2023-08-08 15:23:13 -0600

    Main: Pruebas de Entradas

---
+++ src/BackEnd/Home/Main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	parser "TSwift/Language/Parser"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/antlr4-go/antlr/v4"
+)
+
+func main() {
+	filePath := "../../../Inputs/Input3.swift"
+	fileData, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		fmt.Println("Error al leer el archivo:", err)
+		return
+	}
+	input := string(fileData)
+	fmt.Println(input)
+	inputStream := antlr.NewInputStream(input)
+	scanner := parser.NewScanner(inputStream)
+	stream := antlr.NewCommonTokenStream(scanner, antlr.TokenDefaultChannel)
+	parser := parser.NewParserParser(stream)
+	antlr.TreesDescendants(parser.Init())
+}

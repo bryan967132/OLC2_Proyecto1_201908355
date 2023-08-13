fad684cc9abdcbc5bbb761c5243f2cba1241dc11
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2023-08-12 22:45:16 -0600

    Tipos

--- src/BackEnd/Classes/Utils/Type.go
+++ src/BackEnd/Classes/Utils/Type.go
@@ -3,12 +3,12 @@ package utils
 type Type int
 
 const (
-	INT     Type = 0
-	FLOAT   Type = 1
-	BOOLEAN Type = 2
-	CHAR    Type = 3
-	STRING  Type = 4
-	NIL     Type = 5
-	VECTOR  Type = 6
-	MATRIX  Type = 7
+	INT Type = iota
+	FLOAT
+	BOOLEAN
+	CHAR
+	STRING
+	NIL
+	VECTOR
+	MATRIX
 )
--- src/BackEnd/Classes/Utils/TypeInst.go
+++ src/BackEnd/Classes/Utils/TypeInst.go
@@ -3,20 +3,20 @@ package utils
 type TypeInst int
 
 const (
-	PRINT         TypeInst = 0
-	INIT_ID       TypeInst = 1
-	ASIGN_ID      TypeInst = 2
-	INIT_FUNCTION TypeInst = 3
-	MAIN          TypeInst = 4
-	BLOCK_INST    TypeInst = 5
-	IF            TypeInst = 6
-	LOOP_FOR      TypeInst = 7
-	LOOP_WHILE    TypeInst = 8
-	LOOP_DOWHILE  TypeInst = 9
-	SWITCH        TypeInst = 10
-	CASE          TypeInst = 11
-	BREAK         TypeInst = 12
-	CONTINUE      TypeInst = 13
-	ADD           TypeInst = 14
-	SUB           TypeInst = 15
+	PRINT TypeInst = iota
+	INIT_ID
+	ASIGN_ID
+	INIT_FUNCTION
+	MAIN
+	BLOCK_INST
+	IF
+	LOOP_FOR
+	LOOP_WHILE
+	LOOP_DOWHILE
+	SWITCH
+	CASE
+	BREAK
+	CONTINUE
+	ADD
+	SUB
 )

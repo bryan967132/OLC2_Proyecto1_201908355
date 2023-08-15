76aaf9ae3d54fb4df2577244d51195cbed0e7bd1
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2023-08-14 23:55:54 -0600

    Print: Corrección

--- src/BackEnd/Classes/Instructions/Print.go
+++ src/BackEnd/Classes/Instructions/Print.go
@@ -2,27 +2,25 @@ package instructions
 
 import (
 	env "TSwift/Classes/Env"
-	abstracts "TSwift/Classes/Interfaces"
+	interfaces "TSwift/Classes/Interfaces"
 	utils "TSwift/Classes/Utils"
 	"fmt"
-	"sort"
 )
 
 type Print struct {
 	Line     int
 	Column   int
 	TypeInst utils.TypeInst
-	Exps     []abstracts.Expression
+	Exps     []interfaces.Expression
 }
 
-func NewPrint(line int, column int, exps []abstracts.Expression) *Print {
+func NewPrint(line int, column int, exps []interfaces.Expression) *Print {
 	return &Print{line, column, utils.PRINT, exps}
 }
 
 func (prt *Print) Exec(env *env.Env) interface{} {
 	value := ""
 	if prt.Exps != nil {
-		sort.Slice(prt.Exps, func(i, j int) bool { return true })
 		for i := 0; i < len(prt.Exps); i++ {
 			value1 := prt.Exps[i].Exec(env)
 			if value != "" {

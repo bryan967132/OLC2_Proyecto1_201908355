dd6e25dc67e5ee965d1846b30ae378b7f4857026
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2023-08-15 23:19:02 -0600

    Env: Mostrar impresiones y errores

--- src/BackEnd/Classes/Env/Env.go
+++ src/BackEnd/Classes/Env/Env.go
@@ -9,17 +9,32 @@ type Env struct {
 	name     string
 }
 
-func (env *Env) SetError(errorD string) {
-	env.Errors = append(env.Errors, errorD)
-}
-
 func NewEnv(previous *Env, name string) *Env {
 	return &Env{[]string{}, []string{}, previous, name}
 }
 
+func (env *Env) SetPrints(print string) {
+	env.Prints = append(env.Prints, print)
+}
+
+func (env *Env) PrintPrints() {
+	fmt.Println("\nTSwift:")
+	if len(env.Prints) > 0 {
+		for _, Print := range env.Prints {
+			fmt.Println(Print)
+		}
+	}
+}
+
+func (env *Env) SetError(errorD string) {
+	env.Errors = append(env.Errors, errorD)
+}
+
 func (env *Env) PrintErrors() {
-	fmt.Println("\nErrores")
-	for _, Error := range env.Errors {
-		fmt.Println(Error)
+	if len(env.Errors) > 0 {
+		fmt.Println("\nERRORES:")
+		for _, Error := range env.Errors {
+			fmt.Println(Error)
+		}
 	}
 }

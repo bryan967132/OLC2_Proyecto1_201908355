5fcaa3dcfab96524a7883894d3cd1ac81b435444
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2023-08-17 00:07:16 -0600

    Symbol

--- src/BackEnd/Classes/Env/Symbol.go
+++ src/BackEnd/Classes/Env/Symbol.go
@@ -4,9 +4,9 @@ import utils "TSwift/Classes/Utils"
 
 type Symbol struct {
 	Value   interface{}
-	id      string
+	Id      string
 	Type    utils.Type
-	arrType utils.Type
+	ArrType utils.Type
 }
 
 func NewSymbol(value interface{}, id string, Type, arrType utils.Type) *Symbol {

b9e92acddb92891a198dc2e2735f0ad8811e4cf0
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2023-08-21 01:07:20 -0600

    Interfaz Instruction: Tipo de retorno de Exec

--- src/BackEnd/Classes/Interfaces/Instruction.go
+++ src/BackEnd/Classes/Interfaces/Instruction.go
@@ -2,10 +2,11 @@ package interfaces
 
 import (
 	env "TSwift/Classes/Env"
+	utils "TSwift/Classes/Utils"
 )
 
 type Instruction interface {
 	LineN() int
 	ColumnN() int
-	Exec(env *env.Env) interface{}
+	Exec(env *env.Env) *utils.ReturnType
 }

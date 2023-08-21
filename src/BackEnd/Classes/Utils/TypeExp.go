c66932206e9d80defe1ae41f82359eab4ac1dedb
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2023-08-21 00:50:53 -0600

    TypeExp y TypeInst a string

--- src/BackEnd/Classes/Utils/TypeExp.go
+++ src/BackEnd/Classes/Utils/TypeExp.go
@@ -1,17 +1,17 @@
 package utils
 
-type TypeExp int
+type TypeExp string
 
 const (
-	PRIMITIVE TypeExp = iota
-	ARITHMETIC_OP
-	LOGIC_OP
-	RELATIONAL_OP
-	ACCESS_ID
-	INC
-	DEC
-	PARAMETER
-	CALL_FUNC
-	RETURN
-	CAST
+	PRIMITIVE     TypeExp = "PRIMITIVE"
+	ARITHMETIC_OP TypeExp = "ARITHMETIC_OP"
+	LOGIC_OP      TypeExp = "LOGIC_OP"
+	RELATIONAL_OP TypeExp = "RELATIONAL_OP"
+	ACCESS_ID     TypeExp = "ACCESS_ID"
+	INC           TypeExp = "INC"
+	DEC           TypeExp = "DEC"
+	PARAMETER     TypeExp = "PARAMETER"
+	CALL_FUNC     TypeExp = "CALL_FUNC"
+	RETURN        TypeExp = "RETURN"
+	CAST          TypeExp = "CAST"
 )
--- src/BackEnd/Classes/Utils/TypeInst.go
+++ src/BackEnd/Classes/Utils/TypeInst.go
@@ -1,22 +1,22 @@
 package utils
 
-type TypeInst int
+type TypeInst string
 
 const (
-	PRINT TypeInst = iota
-	INIT_ID
-	ASIGN_ID
-	INIT_FUNCTION
-	MAIN
-	BLOCK_INST
-	IF
-	LOOP_FOR
-	LOOP_WHILE
-	GUARD
-	SWITCH
-	CASE
-	BREAK
-	CONTINUE
-	ADD
-	SUB
+	PRINT         TypeInst = "PRINT"
+	INIT_ID       TypeInst = "INIT_ID"
+	ASIGN_ID      TypeInst = "ASIGN_ID"
+	INIT_FUNCTION TypeInst = "INIT_FUNCTION"
+	MAIN          TypeInst = "MAIN"
+	BLOCK_INST    TypeInst = "BLOCK_INST"
+	IF            TypeInst = "IF"
+	LOOP_FOR      TypeInst = "LOOP_FOR"
+	LOOP_WHILE    TypeInst = "LOOP_WHILE"
+	GUARD         TypeInst = "GUARD"
+	SWITCH        TypeInst = "SWITCH"
+	CASE          TypeInst = "CASE"
+	BREAK         TypeInst = "BREAK"
+	CONTINUE      TypeInst = "CONTINUE"
+	ADD           TypeInst = "ADD"
+	SUB           TypeInst = "SUB"
 )

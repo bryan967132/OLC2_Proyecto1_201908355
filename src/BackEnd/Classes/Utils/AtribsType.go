b1e5f939f5ba32f4a94909c3851e14d6e4e0fafa
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2023-08-22 23:48:18 -0600

    Tipos: Distinción Primitivo o Compuesto

--- src/BackEnd/Classes/Utils/AtribsType.go
+++ src/BackEnd/Classes/Utils/AtribsType.go
@@ -1,11 +1,12 @@
 package utils
 
 type AttribsType struct {
-	Line   int
-	Column int
-	Value  interface{}
+	Line        int
+	Column      int
+	Value       interface{}
+	IsPrimitive bool
 }
 
-func NewAttribsType(line, column int, value interface{}) *AttribsType {
-	return &AttribsType{line, column, value}
+func NewAttribsType(line, column int, value interface{}, isPrimitive bool) *AttribsType {
+	return &AttribsType{line, column, value, isPrimitive}
 }

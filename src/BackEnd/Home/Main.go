561251cd6e1a1102ad3ee55344a7d00166fcdaee
bryan967132 <bryan.tejaxun@gmail.com>
2023-09-05 14:53:34 -0600

    Graficación de CST, Escuchador de Errores

--- src/BackEnd/Home/Main.go
+++ src/BackEnd/Home/Main.go
@@ -7,12 +7,13 @@ import (
 	parser "TSwift/Language/Parser"
 	"fmt"
 	"io/ioutil"
+	"strings"
 
 	"github.com/antlr4-go/antlr/v4"
 )
 
 func main() {
-	filePath := "../../../Inputs/Input15.swift"
+	filePath := "../../../Inputs/Input16.swift"
 	fileData, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		fmt.Println("Error al leer el archivo:", err)
@@ -23,15 +24,60 @@ func main() {
 	scanner := parser.NewScanner(inputStream)
 	tokens := antlr.NewCommonTokenStream(scanner, antlr.TokenDefaultChannel)
 	parser := parser.NewParserParser(tokens)
+	parser.RemoveErrorListeners()
+	parserErrors := listener.NewTSwfitErrorListener()
+	parser.AddErrorListener(parserErrors)
+
 	parser.BuildParseTrees = true
 	tree := parser.Init()
 	var listener *listener.TSwfitListener = listener.NewTSwfitListener()
 	antlr.ParseTreeWalkerDefault.Walk(listener, tree)
-	execute := listener.Code
+
+	//fmt.Println(TreeDot(tree, parser.RuleNames))
+
 	global := env.NewEnv(nil, "Global")
+	for _, fail := range parserErrors.Errors {
+		global.SetError(fmt.Sprintf("%v. %v:%v", Replaces(fail.Msg), fail.Line, fail.Column))
+	}
+
+	execute := listener.Code
 	for _, instruction := range execute {
+		defer func() {
+			if r := recover(); r != nil {
+				global.SetError(fmt.Sprintf("%v", r))
+				global.PrintPrints()
+				global.PrintErrors()
+			}
+		}()
 		instruction.(interfaces.Instruction).Exec(global)
 	}
 	global.PrintPrints()
 	global.PrintErrors()
+}
+
+func Replaces(msg string) string {
+	replaces := [][]string{{"mismatched input", "Inesperado:"}, {"expecting", ". Se esperaba:"}, {"missing", "Inesperado:"}, {"at", "en"}}
+	for _, m := range replaces {
+		msg = strings.Replace(msg, m[0], m[1], -1)
+	}
+	return msg
+}
+
+func TreeDot(tree antlr.Tree, ruleNames []string) string {
+	dot := "digraph Tree {\n\tedge[dir=none];"
+	dot += NodesTree("i", tree, ruleNames)
+	dot += "\n}"
+	return dot
+}
+
+func NodesTree(id string, tree antlr.Tree, ruleNames []string) string {
+	s := antlr.TreesGetNodeText(tree, ruleNames, nil)
+	s = antlr.EscapeWhitespace(s, false)
+	c := tree.GetChildCount()
+	res := "\n\tnodo_" + id + "[label=\"" + strings.Replace(s, "\"", "\\\"", -1) + "\"];"
+	for i := 0; i < c; i++ {
+		res += NodesTree(fmt.Sprintf("%s_%v", id, i), tree.GetChild(i), ruleNames)
+		res += "\n\tnodo_" + id + " -> " + "nodo_" + fmt.Sprintf("%s_%v", id, i) + ";"
+	}
+	return res
 }
